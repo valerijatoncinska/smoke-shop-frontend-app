264b46d0518d18bc99ac4626fc76f381f9500037
alerskij <derim.yavorskiy@gmail.com>
2024-08-12 19:14:22 +0300

    Added API to UserProfilePage

--- src/components/UserProfilePage/UserProfilePage.tsx
+++ src/components/UserProfilePage/UserProfilePage.tsx
@@ -40,7 +40,7 @@ const UserProfilePage: React.FC = () => {
     if (userData) {
       try {
         const response = await axios.put(
-          `/api/user/profile`,
+          `/api/author/profile`,
           {
             name: userData.name,
             street: userData.street,
