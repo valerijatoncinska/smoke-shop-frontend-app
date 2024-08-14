931ce81b24ac1be055a409cb740a5b14c8cacf02
Gorishok <dasa999916@gmail.com>
2024-08-14 15:50:42 +0200

    init

--- src/components/CatalogProductPage/ProductCardPage.tsx
+++ src/components/CatalogProductPage/ProductCardPage.tsx
@@ -1,33 +1,45 @@
-import React from "react";
-import { Product } from "store/redux/productSlice";
-import styles from './ProductCardPage.module.css';
-import { useNavigate } from "react-router-dom";
+import React from "react"
+import { Product } from "store/redux/productSlice"
+import styles from "./ProductCardPage.module.css"
+import { useNavigate } from "react-router-dom"
+import { tobaccoImages } from "../../constans/tobaccoImg"
 
 interface ProductCardPageProps {
-  product: Product;
+  product: Product
 }
 
 const ProductCardPage: React.FC<ProductCardPageProps> = ({ product }) => {
-    const navigate = useNavigate();
+  const navigate = useNavigate()
 
-    const handleViewDetails = () => {
-      navigate(`/products/${product.id}`);
-    }
+  const handleViewDetails = () => {
+    navigate(`/products/${product.id}`)
+  }
+
+  const productImage =
+    product.id > 0 && product.id <= tobaccoImages.length
+      ? tobaccoImages[product.id - 1]
+      : undefined
 
   return (
     <div className={styles.card}>
-      <div className={styles.cardBody}>
-        <h5 className={styles.cardTitle}>{product.title}</h5>
-        <p className={styles.cardText}>{product.price} $</p>
+  <div className={styles.cardImage}>
+    {productImage ? (
+      <img src={productImage} alt={product.title} />
+    ) : (
+      <div className={styles.noImagePlaceholder}>
+        No Image Available
       </div>
-      <div className={styles.cardButton}>
-        <button 
-        className={styles.cardButtonText}
-        onClick={handleViewDetails}
-        >View Details</button>
-      </div>
-    </div>
-  );
-};
+    )}
+  </div>
+  <div className={styles.cardBody}>
+    <h5 className={styles.cardTitle}>{product.title}</h5>
+    <p className={styles.cardPrice}>{product.price} $</p>
+  </div>
+  <div className={styles.cardButton}>
+    <button className={styles.cardButtonText} onClick={handleViewDetails}>View Details</button>
+  </div>
+</div>
+  )
+}
 
-export default ProductCardPage;
+export default ProductCardPage
