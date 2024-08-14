4c35fbe54fa5ca6070877a968b1340b2932d2e60
Gorishok <dasa999916@gmail.com>
2024-08-14 16:32:27 +0200

    adding pictures and fixing errors

--- src/constans/tobaccoImg.ts
+++ src/constans/tobaccoImg.ts
@@ -1,6 +1,6 @@
 import tob1 from '../../public/img/tob1.jpg';
 import tob2 from '../../public/img/tob2.jpg';
 import tob3 from '../../public/img/tob3.jpg';
-// import tob4 from '../../public/img/tob4.jpg';
+import tob4 from '../../public/img/tob4.jpg';
 
-export const tobaccoImages: string[] = [tob1, tob2, tob3];
+export const tobaccoImages: string[] = [tob1, tob2, tob3, tob4];
