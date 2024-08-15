7f0e67e0d179e59f02345a8e9bc99768849afc5d
alerskij <derim.yavorskiy@gmail.com>
2024-08-15 12:42:39 +0300

    убрал возможность редактирования

--- src/components/UserProfilePage/UserProfilePage.tsx
+++ src/components/UserProfilePage/UserProfilePage.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../../store/store";
-import { updateUser, logoutUser, clearError } from "../../store/redux/userSlice";
+import { logoutUser, clearError } from "../../store/redux/userSlice";
 import axios from "axios";
 import "./UserProfilePage.css";
 
@@ -24,13 +24,15 @@ const UserProfilePage: React.FC = () => {
   const status = useSelector((state: RootState) => state.user.status);
   const error = useSelector((state: RootState) => state.user.error);
 
-  const [isEditing, setIsEditing] = useState<boolean>(false);
   const [userData, setUserData] = useState<User | null>(null);
 
   useEffect(() => {
     const fetchUserData = async () => {
       try {
         const response = await axios.get("/api/address", {
+          headers: {
+            Authorization: `Bearer ${user?.accessToken}`,
+          },
         });
 
         if (response.status === 200) {
@@ -52,54 +54,6 @@ const UserProfilePage: React.FC = () => {
       fetchUserData();
     }
   }, [user]);
-
-  const handleEdit = () => {
-    setIsEditing(true);
-  };
-
-  const handleSave = async () => {
-    if (userData) {
-      try {
-        const response = await axios.put(
-          `/api/address/}`,
-          {
-            name: userData.name,
-            street: userData.street,
-            house: userData.house,
-            postalCode: userData.postalCode,
-            city: userData.city,
-            phone: userData.phone,
-          },
-        );
-
-        if (response.status === 200) {
-          const updatedUser = { ...userData, ...response.data };
-
-          // Обновляем состояние в Redux
-          dispatch(updateUser(updatedUser));
-
-          // Обновляем локальное состояние
-          setUserData(updatedUser);
-
-          setIsEditing(false);
-          alert("Profile updated successfully!");
-        } else {
-          console.error("Failed to update profile.");
-        }
-      } catch (error) {
-        console.error("Error updating profile:", error);
-        alert("Error updating profile. Please try again later.");
-      }
-    }
-  };
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setUserData((prevUser) => {
-      if (!prevUser) return null;
-      return { ...prevUser, [name]: value };
-    });
-  };
 
   const handleLogout = async () => {
     await dispatch(logoutUser());
@@ -128,72 +82,16 @@ const UserProfilePage: React.FC = () => {
       <div className="profile-container">
         <div className="profile-section">
           <h2>Details</h2>
-          {isEditing ? (
-            <>
-              <input
-                type="text"
-                name="name"
-                value={userData.name || ""}
-                placeholder="Name"
-                onChange={handleChange}
-              />
-              <input
-                type="text"
-                name="city"
-                value={userData.city || ""}
-                placeholder="City"
-                onChange={handleChange}
-              />
-              <input
-                type="text"
-                name="postalCode"
-                value={userData.postalCode || ""}
-                placeholder="Postal Code"
-                onChange={handleChange}
-              />
-              <input
-                type="text"
-                name="street"
-                value={userData.street || ""}
-                placeholder="Street"
-                onChange={handleChange}
-              />
-              <input
-                type="text"
-                name="house"
-                value={userData.house || ""}
-                placeholder="House Number"
-                onChange={handleChange}
-              />
-              <input
-                type="text"
-                name="phone"
-                value={userData.phone || ""}
-                placeholder="Phone Number"
-                onChange={handleChange}
-              />
-              <button onClick={handleSave}>Save</button>
-              <button onClick={() => setIsEditing(false)}>Cancel</button>
-            </>
-          ) : (
-            <>
-              <p>Name: {userData.name || "N/A"}</p>
-              <p>Email: {userData.email}</p>
-              <p>City: {userData.city || "N/A"}</p>
-              <p>Postal code: {userData.postalCode || "N/A"}</p>
-              <p>Street: {userData.street || "N/A"}</p>
-              <p>House number: {userData.house || "N/A"}</p>
-              <p>Phone number: {userData.phone || "N/A"}</p>
-            </>
-          )}
+          <p>Name: {userData.name || "N/A"}</p>
+          <p>Email: {userData.email}</p>
+          <p>City: {userData.city || "N/A"}</p>
+          <p>Postal code: {userData.postalCode || "N/A"}</p>
+          <p>Street: {userData.street || "N/A"}</p>
+          <p>House number: {userData.house || "N/A"}</p>
+          <p>Phone number: {userData.phone || "N/A"}</p>
         </div>
       </div>
       <div className="buttons">
-        {!isEditing && (
-          <button className="edit-profile" onClick={handleEdit}>
-            Edit profile
-          </button>
-        )}
         <button className="logout" onClick={handleLogout}>
           Logout
         </button>
