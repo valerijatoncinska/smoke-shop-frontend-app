f222cf618ef19ec65d45b0851020e8b34120bc1f
alerskij <derim.yavorskiy@gmail.com>
2024-08-15 11:39:50 +0300

    Попытка номер №100

--- src/components/UserProfilePage/UserProfilePage.tsx
+++ src/components/UserProfilePage/UserProfilePage.tsx
@@ -31,9 +31,6 @@ const UserProfilePage: React.FC = () => {
     const fetchUserData = async () => {
       try {
         const response = await axios.get("/api/address", {
-          headers: {
-            Authorization: `Bearer ${user?.accessToken}`,
-          },
         });
 
         if (response.status === 200) {
@@ -73,11 +70,6 @@ const UserProfilePage: React.FC = () => {
             city: userData.city,
             phone: userData.phone,
           },
-          {
-            headers: {
-              Authorization: `Bearer ${userData.accessToken}`,
-            },
-          }
         );
 
         if (response.status === 200) {
