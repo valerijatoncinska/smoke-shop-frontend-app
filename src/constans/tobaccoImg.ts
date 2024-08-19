02a516a6f218dd3f003fe2b190aa0bab7f180ea5
Gorishok <dasa999916@gmail.com>
2024-08-19 16:23:39 +0200

    fixed profile and catalog

--- src/constans/tobaccoImg.ts
+++ src/constans/tobaccoImg.ts
@@ -6,6 +6,9 @@ import tob5 from '../../public/img/tob5.jpg';
 import tob6 from '../../public/img/tob6.jpg';
 import tob7 from '../../public/img/tob7.jpg';
 import tob8 from '../../public/img/tob8.jpg';
+import tob9 from '../../public/img/tob9.jpg';
+import tob10 from '../../public/img/tob10.jpg';
+import tob11 from '../../public/img/tob11.jpg';
 
 
-export const tobaccoImages: string[] = [tob1, tob2, tob3, tob4, tob5, tob6, tob7, tob8];
+export const tobaccoImages: string[] = [tob1, tob2, tob3, tob4, tob5, tob6, tob7, tob8, tob9, tob10, tob11];
