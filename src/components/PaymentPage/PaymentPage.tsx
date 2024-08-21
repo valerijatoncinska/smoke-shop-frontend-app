5613ce2fbc4f3dbc5fd32cfc8ad30303cb1814d3
danbaliaba <danbaliab@gmail.com>
2024-08-21 18:57:12 +0200

    Added payments for cart

--- src/components/PaymentPage/PaymentPage.tsx
+++ src/components/PaymentPage/PaymentPage.tsx
@@ -1,17 +1,18 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import './PaymentPage.css';
+import React, { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import "./PaymentPage.css"
+import axios from "axios"
 
 const PaymentPage: React.FC = () => {
-  const [cardNumber, setCardNumber] = useState('');
-  const [expiryDate, setExpiryDate] = useState('');
-  const [cvv, setCvv] = useState('');
-  const navigate = useNavigate();
+  const [cardNumber, setCardNumber] = useState("")
+  const [expiryDate, setExpiryDate] = useState("")
+  const [cvv, setCvv] = useState("")
+  const navigate = useNavigate()
 
-  const handleSubmit = (event: React.FormEvent) => {
-    event.preventDefault();
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault()
     // Send payment details to the backend and get order details in response
-    fetch('/api/checkout', {
+    /* fetch('/api/checkout', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -25,8 +26,37 @@ const PaymentPage: React.FC = () => {
     })
     .catch(error => {
       console.error('Error:', error);
-    });
-  };
+    }); */
+
+    try {
+      // Получаем адрес пользователя
+      const response = await axios.get("/api/address").then(response => {console.log(response.data)
+        return response
+      }
+      )
+      const addressData = response.data[0]
+
+      // После того как состояние userAddress обновится, можно использовать его
+      if (addressData && addressData.id) {
+        const data = {
+          payments: "paypal",
+          deliveryAddress: addressData.id, // Используем значение id из ответа
+          billingAddress: addressData.id,
+        }
+        console.log(data);
+        
+
+        await axios
+          .post("/api/cart/order", data)
+          .then(response => console.log(response.data))
+          .catch(error => console.log(error))
+        // Переход на страницу подтверждения заказа
+        navigate("/order-history")
+      }
+    } catch (error) {
+      console.log("Error:", error)
+    }
+  }
 
   return (
     <div className="payment-page">
@@ -38,7 +68,7 @@ const PaymentPage: React.FC = () => {
             <input
               type="text"
               value={cardNumber}
-              onChange={(e) => setCardNumber(e.target.value)}
+              onChange={e => setCardNumber(e.target.value)}
               required
             />
           </label>
@@ -49,7 +79,7 @@ const PaymentPage: React.FC = () => {
             <input
               type="text"
               value={expiryDate}
-              onChange={(e) => setExpiryDate(e.target.value)}
+              onChange={e => setExpiryDate(e.target.value)}
               required
             />
           </label>
@@ -60,19 +90,21 @@ const PaymentPage: React.FC = () => {
             <input
               type="text"
               value={cvv}
-              onChange={(e) => setCvv(e.target.value)}
+              onChange={e => setCvv(e.target.value)}
               required
             />
           </label>
         </div>
-        <div className="buttons mt-4">
-          <button type="submit" className="pay-button" onClick={() => navigate("/order-history")}>Pay</button>
-          <button type="button" className="paypal-button">PayPal</button>
+        <div className="buttons mt-4 d-flex justify-content-center">
+          {/* <button type="submit" className="pay-button" onClick={() => navigate("/order-history")}>Pay</button> */}
+          <button type="submit" className="paypal-button">
+            PayPal
+          </button>
           {/* <button type="button" className="gpay-button">GPay</button> */}
         </div>
       </form>
     </div>
-  );
+  )
 }
 
-export default PaymentPage;
+export default PaymentPage
